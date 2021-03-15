af90177b50836639d474f7c72c9a12c1d2612613
Tom Coleman <tom@thesnail.org>
2021-03-15 12:45:38 +1100

    Simplify arguments to `finishJob`

--- lib/lua/finishJob.lua
+++ lib/lua/finishJob.lua
@@ -11,11 +11,10 @@ key 8 -> bq:name:events
 key 9 -> bq:name:jobs
 
 arg 1 -> job id
-arg 2 -> remove job?
-arg 3 -> job data
-arg 4 -> new job status
-arg 5 -> delayed time
-arg 6 -> job event data
+arg 2 -> job data
+arg 3 -> new job status
+arg 4 -> delayed time
+arg 5 -> job event data
 --]]
 
 -- If we don't find the job in the active list, ensure it hasn't been recently stalled
@@ -25,23 +24,23 @@ end
 
 redis.call("srem", KEYS[3], ARGV[1])
 
-if ARGV[2] ~= "" then -- remove?
+if ARGV[2] == "" then -- remove if no job data sent
   redis.call("hdel", KEYS[9], ARGV[1])
 else
-  redis.call("hset", KEYS[9], ARGV[1], ARGV[3])
+  redis.call("hset", KEYS[9], ARGV[1], ARGV[2])
 
-  if ARGV[4] == "failed" then
+  if ARGV[3] == "failed" then
     redis.call("sadd", KEYS[4], ARGV[1])
-  elseif ARGV[4] == "succeeded" then
+  elseif ARGV[3] == "succeeded" then
     redis.call("sadd", KEYS[7], ARGV[1])
-  elseif ARGV[5] ~= "" then
-    redis.call("zadd", KEYS[5], tonumber(ARGV[5]), ARGV[1])
-    redis.call("publish", KEYS[6], tonumber(ARGV[5]))
+  elseif ARGV[4] ~= "" then
+    redis.call("zadd", KEYS[5], tonumber(ARGV[4]), ARGV[1])
+    redis.call("publish", KEYS[6], tonumber(ARGV[4]))
   else
     redis.call("lpush", KEYS[2], ARGV[1])
   end
 end
 
-if ARGV[6] ~= "" then
-  redis.call("publish", KEYS[8], ARGV[6]);
+if ARGV[5] ~= "" then
+  redis.call("publish", KEYS[8], ARGV[5]);
 end
