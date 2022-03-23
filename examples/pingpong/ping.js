d0c8164938f57bb76241ca8c1a311c03e42b2adc
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2022-03-23 13:33:49 +0100

    Do not report progress forever

--- examples/pingpong/ping.js
+++ examples/pingpong/ping.js
@@ -6,9 +6,15 @@ const sendPing = async function () {
   console.log('created job, will remove after 1sec');
   let i = 0;
 
-  setInterval(() => {
-    job.reportProgress(i++);
-  }, 1000);
+  const report = () => {
+    if (i < 5) {
+      console.log('report progress');
+      job.reportProgress(i++);
+      setTimeout(report, 1000);
+    }
+  };
+
+  report();
 
   setTimeout(() => {
     console.log('removing job');
